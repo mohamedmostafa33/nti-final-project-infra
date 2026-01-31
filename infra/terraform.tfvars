7696f18b4d7eddd2b5f1a1f2b3bec3cb09e187ca
mohamedmostafa33 <mohamedmostafaah1@gmail.com>
2026-01-31 15:10:53 +0200

    feat: integrate S3 module with root configuration
    - Add S3 module to main infrastructure configuration
    - Define S3 bucket name variable and default value
    - Export S3 bucket name as root output
    - Update naming convention to reddit-clone project

--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,1 +1,3 @@
 ecr_name = "reddit-clone-app-repo"
+
+s3_bucket_name = "reddit-clone-s3-media"
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,11 @@
 variable "ecr_name" {
   description = "Name of the ECR repository"
   type        = string
-  default     = "puddle-app-repo"
+  default     = "reddit-clone-app-repo"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the s3 bucket"
+  type        = string
+  default     = "reddit-clone-s3-media"
 }
